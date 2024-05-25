7bdccb38553bc3ca3d0879581f42e0f391ca8ace
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-24 22:22:50 -0300

    feat: estrutura inicial da api

---
+++ src/fetch-data-recipe.js
@@ -0,0 +1,79 @@
+import puppeteer from "puppeteer";
+
+const url = "https://www.receiteria.com.br/receitas-de-jantar-simples/";
+
+export async function main(indexStart, indexEnd) {
+  try {
+    // constante das receitas
+    const dataRecipes = [];
+
+    // Criando uma instância do chrome
+    const browser = await puppeteer.launch({
+      headless: true,
+      devtools: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    // Criando uma pagina
+    const page = await browser.newPage();
+
+    // Acessando a pagina das receitas
+    await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
+
+    // Selecionando apenas os links dos cards de cada receita
+    const links = await page.$$eval(".shadow-sm", (el) =>
+      el.map((link) => link.href)
+    );
+
+    const linksTeste = links.slice(indexStart, indexEnd);
+
+    for (const link of linksTeste) {
+      // Acessando varias paginas dinâmicamente
+      await page.goto(link, { waitUntil: "networkidle2", timeout: 60000 });
+
+      // Para cada pagina criar um objeto com as informações necessárias
+      const newRecipe = await page.evaluate(() => {
+        const urlImage = document
+          .querySelector(".superimg > img")
+          ?.getAttribute("src");
+        const name = document.querySelector(".title > h1")?.innerText;
+        const portion =
+          document.querySelectorAll(".align-middle")[0]?.innerText;
+        const timer = document.querySelectorAll(".align-middle")[1]?.innerText;
+        const elIngredients = document.querySelectorAll(
+          ".ingredientes > ul > li > label"
+        );
+        const ingredients = [];
+        for (let label of elIngredients) {
+          ingredients.push(label.innerText);
+        }
+
+        const elPreparations = document.querySelectorAll(
+          ".lista-preparo-1 > li > span"
+        );
+        const preparations = [];
+        for (let span of elPreparations) {
+          preparations.push(span.innerText);
+        }
+
+        return {
+          urlImage,
+          name,
+          portion,
+          timer,
+          ingredients,
+          preparations,
+        };
+      });
+
+      dataRecipes.push(newRecipe);
+    }
+
+    return dataRecipes;
+  } catch (error) {
+    return error;
+  } finally {
+    // Fechando o chrome para evitar vazamento de memória
+    await browser.close();
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { FetchDataRecipe } from "./fetch-data-recipe.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "running" });
+});
+
+app.get("/data", async (req, res) => {
+  const { indexStart, indexEnd } = req.query;
+
+  try {
+    // TODO resolver error do browser is not defined
+    const response = await FetchDataRecipe(indexStart, indexEnd);
+    console.log(response);
+    res.status(200).json({ dataRecipe: response });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json(error);
+  }
+});
+
+const PORT = 9008;
+
+app.listen(PORT, () => {
+  console.log(`running in http://localhost:${PORT}`);
+});
