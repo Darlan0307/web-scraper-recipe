6d038d6487d3418361ddf4f207a5ecd532ceb198
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-08 15:51:03 -0300

    feat: adicionando a dependência cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { FetchDataRecipe } from "../src/fetch-data-recipe.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
